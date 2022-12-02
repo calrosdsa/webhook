eac03b694c1f42671b2b2d2f205abb4833db6808
calrosdsa <carlosduram71@gmail.com>
2022-12-02 14:09:26 -0400

    changes

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ export default function Home() {
         if (response.authResponse) {
          console.log('Welcome!  Fetching your information.... ');
          FB.api('/me', function(response) {
+          console.log(response)
            console.log('Good to see you, ' + response.name + '.');
          });
         } else {
