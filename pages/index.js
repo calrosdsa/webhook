ec1a250df76c7b068bf1603ccf7303bac422e3ec
calrosdsa <carlosduram71@gmail.com>
2022-12-02 16:32:30 -0400

    changes

--- pages/index.js
+++ pages/index.js
@@ -14,9 +14,18 @@ export default function Home() {
       window.FB.login(function(response) {
         if (response.authResponse) {
          console.log('Welcome!  Fetching your information.... ');
-         FB.api('/me', function(response) {
-           console.log('Good to see you, ' + response.name + '.');
-         });
+         FB.api(
+          '/me',
+          'GET',
+          {"fields":"email,name"},
+          function(response) {
+              // Insert your code here
+              console.log(response)
+          }
+        );
+        //  FB.api('/me', function(response) {
+        //    console.log('Good to see you, ' + response.name + '.');
+        //  });
         } else {
          console.log('User cancelled login or did not fully authorize.');
         }
