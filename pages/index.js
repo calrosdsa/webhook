8bfe34757f78b1d65dc9cf5523637fd71bd64971
calrosdsa <carlosduram71@gmail.com>
2022-12-02 15:39:54 -0400

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,52 @@
 import { useEffect,useState } from "react";
 import { useCookies } from "react-cookie";
-
 export default function Home() {
   const [cookies, setCookie, removeCookie] = useCookies(['cookie-name']);
 
-  const onLoginClick = () => {
-    if (typeof window !== 'undefined') {
-      window.FB.login(function(response) {
-        if (response.authResponse) {
-         console.log('Welcome!  Fetching your information.... ');
-         FB.api('/me', function(response) {
-          console.log(response)
-          setCookie("name",response.name,{
-            path:'/',
-            maxAge:60*5
-          })
-          setCookie("id",response.id,{
-            path:'/',
-            maxAge:60*5
-          })
-           console.log('Good to see you, ' + response.name + '.');
-         });
-        } else {
-         console.log('User cancelled login or did not fully authorize.');
-        }
-    });
-  };
+  useEffect(() => {
+    InitFacebook()
+  }, []);
+
+  return (
+      <div><button onClick={LoginFacebook()}>Login with Facebook</button></div>
+  );
+
+};
+
+const LoginFacebook = () => {
+  if (typeof window !== 'undefined') {
+    // FB.api(
+    //   '/me',
+    //   'GET',
+    //   {"fields":"email,gender,location"},
+    //   function(response) {
+    //     console.log(response)
+    //   }
+    // );
+    window.FB.login(function(response) {
+      if (response.authResponse) {
+       console.log('Welcome!  Fetching your information.... ');
+       FB.api('/me', function(response) {
+        console.log(response)
+        setCookie("name",response.name,{
+          path:'/',
+          maxAge:60*5
+        })
+        setCookie("id",response.id,{
+          path:'/',
+          maxAge:60*5
+        })
+         console.log('Good to see you, ' + response.name + '.');
+       });
+      } else {
+       console.log('User cancelled login or did not fully authorize.');
+      }
+  });
+};
 }
 
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
+const InitFacebook = ()=>{
+  if (typeof window !== 'undefined') {
       window.fbAsyncInit = () => {
           window.FB.init({
               appId            : '694102408690400',
@@ -37,19 +54,13 @@ export default function Home() {
               xfbml            : true,
               version          : 'v11.0'
           });
-      };
-    }
-      (function (d, s, id) {
+        };
+        (function (d, s, id) {
           var js, fjs = d.getElementsByTagName(s)[0];
           if (d.getElementById(id)) { return; }
           js = d.createElement(s); js.id = id;
           js.src = "https://connect.facebook.net/en_US/sdk.js";
           fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
-  }, []);
-
-  return (
-      <div><button onClick={onLoginClick}>Login with Facebook</button></div>
-  );
-
-};
+        }(document, 'script', 'facebook-jssdk'));
+      }
+}
