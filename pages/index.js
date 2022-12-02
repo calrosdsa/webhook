0d322aa8133779f1921e90b31d886b09538337a9
calrosdsa <carlosduram71@gmail.com>
2022-12-02 14:01:58 -0400

    changes

---
+++ pages/index.js
@@ -0,0 +1,49 @@
+import { getProviders,signIn ,useSession} from "next-auth/react"
+import { useEffect,useState } from "react";
+import FacebookLogin from 'react-facebook-login';
+
+export default function Home() {
+  // const [session, loading] = useSession()
+  // const [providers, setProviders] = useState({});
+  const [isLoggedin, setIsLoggedin] = useState(false);
+
+  const onLoginClick = () => {
+    if (typeof window !== 'undefined') {
+      window.FB.login(function(response) {
+        if (response.authResponse) {
+         console.log('Welcome!  Fetching your information.... ');
+         FB.api('/me', function(response) {
+           console.log('Good to see you, ' + response.name + '.');
+         });
+        } else {
+         console.log('User cancelled login or did not fully authorize.');
+        }
+    });
+  };
+}
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.fbAsyncInit = () => {
+          window.FB.init({
+              appId            : '694102408690400',
+              autoLogAppEvents : true,
+              xfbml            : true,
+              version          : 'v11.0'
+          });
+      };
+    }
+      (function (d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) { return; }
+          js = d.createElement(s); js.id = id;
+          js.src = "https://connect.facebook.net/en_US/sdk.js";
+          fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+  }, []);
+
+  return (
+      <div><button onClick={onLoginClick}>Login with Facebook</button></div>
+  );
+
+};
