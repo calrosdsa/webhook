87b06984308b21376b1ac76f2e10e52337a3d708
calrosdsa <carlosduram71@gmail.com>
2022-12-06 15:32:51 -0400

    changes

--- context/selectors.ts
+++
@@ -1,9 +0,0 @@
-import { createSelector } from '@reduxjs/toolkit';
-import { RootState } from './store';
-
-export const showSideBar = (state: RootState) => state.ui.showSideBar;
-export const getRole = (state:RootState) =>state.auth.user.role
-export const getAuthenticated = (state:RootState) => state.auth.isAuthenticated
-export const getUser = (state:RootState) => state.auth.user
-
-// export const countSelector = createSelector(showSideBar, state => state);
