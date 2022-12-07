0526fa3cc44573477958cf8cf688d82a2835dc3a
calrosdsa <carlosduram71@gmail.com>
2022-12-07 11:55:40 -0400

    changes

--- pages/api/webhooks.ts
+++ pages/api/webhooks.ts
@@ -5,7 +5,7 @@ export default async(req:NextApiRequest,res:NextApiResponse)=>{
   try {
     const text = JSON.stringify(req.body)
     try{
-    const sendData =await axios.post("https://6e44-200-87-209-44.sa.ngrok.io/post/",{text})
+    const sendData =await axios.post("https://a362-200-87-209-44.sa.ngrok.io/post/",{text})
     sendData
     }catch(err){
         res.status(500).send({ error: 'failed to fetch data' })
