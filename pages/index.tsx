0965c3cbd8af9b42f84a2882625d762538076f89
calrosdsa <carlosduram71@gmail.com>
2022-12-08 17:20:48 -0400

    changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,13 +30,16 @@ export default function Home() {
 
   useEffect(() => {
 
+     
+    
     // console.log(id)
     //https://teclu.com/validatelike.php?id=113209743565830
-    // async function fetchMyAPI() {
-    //   const response = await axios.get(`https://teclu.com/validatelike.php?id=${id}`)
-    //   console.log(response)
-    // }
-    // fetchMyAPI()
+    async function fetchMyAPI() {
+      // const response = await axios.get(`https://teclu.com/validatelike.php?id=${id}`)
+      const response = await axios.get('https://graph.facebook.com/v15.0/111941735089630?fields=feed%7Blikes%7D&access_token=EAALZALdJy4pQBAMDjZBJHGSZBIrFVG5tv6JENZBY87g6HwLnS6ZCaCjWfBJotpZABWXuv53tbRZBgdANdup5RpLWZBSw0K6ltVomA10cHVrtQ4vHakUY0alGmm0gPYLzuHiZAnVTncsX9q3Wqz3ZBjSPpMrP75A8HF7ooNMvOrSj6U9qB6Gxw2gYTzZBRzNPPHo9U4ZD')
+      console.log(response)
+    }
+    fetchMyAPI()
     if (typeof window !== 'undefined') {
       const parsed = queryString.parse(window.location.search)
       console.log(parsed)
@@ -73,3 +76,7 @@ export default function Home() {
 
 
 //https://76.76.21.241/?cmd=login&mac=ac:12:03:9c:ae:05&essid=radius&ip=172.31.98.4&apname=40%3Ae3%3Ad6%3Acc%3A62%3Aa8&apmac=40%3Ae3%3Ad6%3Acc%3A62%3Aa8&vcname=SetMeUp-CC%3A62%3AA8&switchip=securelogin.arubanetworks.com&url=http%3A%2F%2Fwww.msftconnecttest.com%2Fredirect
+
+//https://community.arubanetworks.com/community-home/digestviewer/viewthread?MID=16648#bmbd935f86-6b5b-4ee5-9a0d-33b606b01d67
+
+//https://login.webhook-murex.vercel.app/swarm.cgi
