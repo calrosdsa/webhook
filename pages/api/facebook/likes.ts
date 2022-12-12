0407f610f8ba8d8885745cff68907de0a2084628
calrosdsa <carlosduram71@gmail.com>
2022-12-12 10:52:22 -0400

    changes

--- pages/api/facebook/likes.ts
+++ pages/api/facebook/likes.ts
@@ -5,7 +5,7 @@ import axios from 'axios';
 // import { json } from "node:stream/consumers";
 export default async(req:NextApiRequest,res:NextApiResponse)=>{
     if(req.method == 'GET'){
-        const response = await axios.get("https://graph.facebook.com/v15.0/111114835172863?fields=feed.limit(1)%7Blikes%7D&access_token=EAALZALdJy4pQBAGtmAKT1Mfm23mnZBdunesa07EI3yJmGv1rs0Q5m8OqzWMT0K7DD5cJgxEYyE64BDVLkh0NkEisesWthjHsxNkZBE94Uk1XDZBEFT9MnZAQVWdAtyQe2KF4wynO4uBlJyOTadrobMD03uPGMdL7CJYUVU12B4vS7dUXan0IZAY1x0n3kttqBK0ZAn7gFBe9aIChLZC8rGTzjjlZBs7YNQNcZD")
+        const response = await axios.get("https://graph.facebook.com/v15.0/111114835172863?fields=feed.limit(1)%7Blikes%7D&access_token=EAALZALdJy4pQBAHVhbOytq6JKMkobeK70MI8sGMeZBcwiPFjHMnq8QVOZB17gDi1Iid0OZCrZAwsEvKL0TWZAU4lZC8iFSHVvBImkUbInKZAmfiwepuODByTE4NNUHCZCwkZAtGar3DEKDmNSEK3ZBzH8M3XJpZBQIPpkR5SCHBvQIhNR0rQt3PZAnAJ5gXOx1zrzbdURnT7ueFMGKQZDZD")
         res.send({
             likes:response.data
         })
