0b5141303507eccded1b21fe5bf158b27db93679
calrosdsa <carlosduram71@gmail.com>
2022-12-13 15:26:30 -0400

    changes

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -61,7 +61,10 @@ const LandingPage = () =>{
        <a href="https://www.freeprivacypolicy.com/live/83964b85-328e-46c5-a236-33e4fd63a5a6" 
        target="_blank" rel="noreferrer" className=" underline text-facebook cursor-pointer font-medium">Politicas de Privacidad</a>
         </div>
-   
+        <form>
+<input type="button" value="New Window!" onClick={()=>window.open("https://www.geeksforgeeks.org","", "width=300, height=300")}
+  className='w-20 h-10 bg-gray-400'/>
+</form> 
         {/* <input type="text" value={url} onChange={(e)=>setUrl(e.target.value)}
           className="w-full"/> */}
   {/* <h1>dmskdaskdksa</h1> */}
