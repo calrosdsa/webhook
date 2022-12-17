2f363a5b1c393f784218ceeef24a8a9593005e8a
calrosdsa <carlosduram71@gmail.com>
2022-12-17 11:23:58 -0400

    changes

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -77,7 +77,7 @@ const LandingPage = ({isAuthenticated,isLoading,authLoading,isMobile,postUrl}:Pr
         {/* <a  href={`intent://webhook-murex.vercel.app?login_url=${loginUrl}&continue_url=${continueUrl}#Intent;scheme=https;end`}  */}
         <span onClick={()=>{
           if(typeof window != 'undefined'){
-            window.open(`https://webhook-murex.vercel.app?login_url=${loginUrl}&continue_url=${continueUrl}`, '_blank');
+            window.open(`https://webhook-murex.vercel.app?login_url=${loginUrl}&continue_url=${continueUrl}`, '_system');
           }
         }}>
           {/* target="_blank" rel='noreferrer'> */}
