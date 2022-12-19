07194a1e1140b5bb63e9fe73504acea533bbccd4
calrosdsa <carlosduram71@gmail.com>
2022-12-19 14:42:45 -0400

    'changes'

---
+++ git-push.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "Hello world!"
+# git add .
+# git commit -m "changes"
+# git push -u origin master
--- pages/api/push.ts
+++ pages/api/push.ts
@@ -6,7 +6,15 @@ import { json } from "stream/consumers";
 // import { json } from "node:stream/consumers";
 export default async(req:NextApiRequest,res:NextApiResponse)=>{
     if(req.method == 'POST'){
-     console.log(req.body)
+        const shell = require('shelljs')
+        // console.log('render')
+        //  if(req.body.pusher.name =="calrosdsa"){
+            shell.exec("git init")
+            shell.exec("git add .")
+            shell.exec("git commit -m 'changes'")
+            shell.exec("git push -u origin main")
+
+    //  }
         res.status(200).json({
             ok:"200"
         })
