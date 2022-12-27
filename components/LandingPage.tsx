f0f323bd2208204e3e83a4b37ff6251a6b47a3d9
calrosdsa <carlosduram71@gmail.com>
2022-12-27 11:26:51 -0400

    changes

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -117,7 +117,7 @@ const LandingPage = ({isAuthenticated,isLoading,authLoading,isMobile,postUrl}:Pr
       <p className="p-4 border-2 border-b-gray-500 text-xs sm:text-sm md:text-base text-center ">
         {/* Para acceder a la red, deberás iniciar sesión con tu cuenta de Facebook y posteriormente
        dar "me gusta" a la última publicación de la página de */}
-       url
+       {url}
         <a href="https://www.facebook.com/Yacimientos/" target={'_blank'} rel='noreferrer'
         className="text-facebook"> YPFB Corp</a>.
       </p>
