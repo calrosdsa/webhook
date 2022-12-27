932942d7a9992bec3f161a15255c9b88ec304e4d
calrosdsa <carlosduram71@gmail.com>
2022-12-27 17:07:16 -0400

    changes

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -42,7 +42,7 @@ const LandingPage = ({isAuthenticated,isLoading,authLoading,isMobile,postUrl}:Pr
         // window.location.replace(
         //    `safari-https://${domain}?login_url=${login}&continue_url=${continueUrl}`
         // );
-      window.location.replace("youtube://");
+      window.location.replace("mobile-safari://");
     }
   }
 
@@ -63,7 +63,7 @@ const LandingPage = ({isAuthenticated,isLoading,authLoading,isMobile,postUrl}:Pr
 
       window.location.replace(url);
     } else if (isIOS) {
-      window.location.replace("google-chrome://");
+      window.location.replace("chrome://");
         // window.location.replace(
         //   "https://apps.apple.com/us/app/instagram/id389801252"
         // );
