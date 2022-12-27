c7ce0271e3d6ec6a8ad0c35f0e400315451e668a
calrosdsa <carlosduram71@gmail.com>
2022-12-27 14:41:00 -0400

    changes

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -62,7 +62,7 @@ const LandingPage = ({isAuthenticated,isLoading,authLoading,isMobile,postUrl}:Pr
 
       window.location.replace(url);
     } else if (isIOS) {
-      window.location.replace("instagram://");
+      window.location.replace("safari://");
         // window.location.replace(
         //   "https://apps.apple.com/us/app/instagram/id389801252"
         // );
