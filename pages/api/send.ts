a0ff00ed29c737fb62d8cbf6af2ec01ea2949d38
calrosdsa <carlosduram71@gmail.com>
2023-01-18 15:15:38 -0400

    changes

--- pages/api/send.ts
+++ pages/api/send.ts
@@ -6,22 +6,25 @@ import axios from 'axios';
 export default async(req:NextApiRequest,res:NextApiResponse)=>{
     // const {username,password,switch_url} = req.body
     if(req.method == 'GET'){
-      fetch ("http://192.0.2.1/login.html",{
-        method:'POST',
-        mode: "no-cors",
-        body:new URLSearchParams("username=marca&password=211120&buttonClicked=4")
-      }).then(res=>{
-        console.log(res)
-        return res
-      }).then(res=>{
-        console.log(res)
+      try{
+        const send = await axios.post("http://192.0.2.1/login.html",{
+          body:new  URLSearchParams("username=marca&password=2011200&buttonClicked=4")
+      },{
+        headers:{
+          'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+        }
       })
-          //  if(sendRequest.status == 200){
-             res.status(200).json({
-              ok:"okk"
-             })
-            // }else{
-              // res.status(500)
-            // }
-    }
+      console.log('sending succesful')
+      console.log(send.status)
+      //  if(sendRequest.status == 200){
+        res.status(200).json({
+          ok:send.data
+        })
+        // }else{
+          // res.status(500)
+          // }
+        }catch(err ){
+          console.log(err)
+        }
+        }
 }
