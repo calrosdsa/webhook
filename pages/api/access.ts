51e64f42a1193e0c307720a8d104193beb13a36e
calrosdsa <carlosduram71@gmail.com>
2023-01-18 15:46:47 -0400

    changes

--- pages/api/access.ts
+++ pages/api/access.ts
@@ -6,7 +6,7 @@ import axios from 'axios';
 export default async(req:NextApiRequest,res:NextApiResponse)=>{
     // const {username,password,switch_url} = req.body
       try{
-        const send = await axios.post("http://192.0.2.1/login.html",{
+        const send = await axios.post("https://192.0.2.1/login.html",{
           body:new  URLSearchParams("username=marca&password=2011200&buttonClicked=4")
       },{
         timeout:1000*40,
